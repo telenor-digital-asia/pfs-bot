0fe2dad5567a0620ec5063f509f5995be71baf0f
Soracha <fai@abctech-thailand.com>
2017-04-16 13:42:18 +0700

    Create endpoint to get current time

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const Moment = require('moment-timezone');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -7,6 +8,12 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
+app.get('/time', (req, res) => {
+    const osloTime = Moment().tz('Europe/Oslo').format('DD MMM YYYY - HH:mm');
+    const bkkTime = Moment().tz('Asia/Bangkok').format('DD MMM YYYY - HH:mm');
+    res.send(`Oslo ${osloTime}\nBangkok ${bkkTime}`);
+});
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
